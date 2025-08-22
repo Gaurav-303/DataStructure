3b91015ac753fc8ce5dd30b65bf351735153707f
Gaurav-303 <gaurav134mishra@gmail.com>
2025-08-22 12:23:01 +0530

    Added Reverse Linked List implementation

---
+++ src/linked_list/Reverse_LinkedList.java
@@ -0,0 +1,37 @@
+package linked_list;
+
+class ListNode {
+    int val;
+    ListNode forward;
+    ListNode(int val) { this.val = val; }
+}
+
+public class Reverse_LinkedList {
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.forward;
+            curr.forward = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.forward = new ListNode(2);
+        head.forward.forward = new ListNode(3);
+        head.forward.forward.forward = new ListNode(4);
+
+        head = reverseList(head);
+
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.forward;
+        }
+    }
+}
+
