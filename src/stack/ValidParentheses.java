66d5a868ff97cd8e872d524b4b2ae5a7f78bf864
Gaurav-303 <gaurav134mishra@gmail.com>
2025-08-25 11:26:34 +0530

    valid Parantheses using stack

---
+++ src/stack/ValidParentheses.java
@@ -0,0 +1,30 @@
+package stack;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+
+                if (c == ')' && top != '(') return false;
+                if (c == '}' && top != '{') return false;
+                if (c == ']' && top != '[') return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String input = "{[()]}";
+        System.out.println(isValid(input));
+    }
+}
+
